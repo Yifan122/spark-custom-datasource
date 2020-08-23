632de4803e1cea261de8bf5891a1c9509694d311
Yifan <zengyifan122@gmail.com>
2020-08-23 11:34:42 +0800

    add some explanation

--- src/main/scala/com/yifan/bigdata/DataSourceV1Example.scala
+++ src/main/scala/com/yifan/bigdata/DataSourceV1Example.scala
@@ -1,19 +1,10 @@
 package com.yifan.bigdata
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 
 object DataSourceV1Example {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("ReadCSV").master("local[2]").getOrCreate()
-    val sc = spark.sparkContext
-
-    val customSchema = StructType(Array(
-      StructField("name", StringType, true),
-      StructField("age", IntegerType, true),
-      StructField("major", StringType, true)
-    ))
-    //    val df = spark.read.format("csv").schema(customSchema).load("src/main/resources/student.csv")
     val df = spark.read.format("csv").load("src/main/resources/student.csv")
     df.show()
   }
